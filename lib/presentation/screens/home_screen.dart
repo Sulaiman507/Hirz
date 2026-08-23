0b39dbf8ae669cea27240a0e18e51119c47d5885
Sulaiman507 <sulaiman507@users.noreply.github.com>
2026-08-23 16:11:51 +0300

    fix: remove unused flutter_animate import

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -4,7 +4,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../core/l10n/app_localizations.dart';
